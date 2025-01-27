9eda216dded91477a6a444d0f43a4f46dbe0567c
Anoop Singh <Reggerfgod@gmail.com>
2025-01-27 18:56:29 +0530

    event and config change

--- src/main/java/com/reggarf/mods/world_first_join_message/World_First_Join_Message.java
+++ src/main/java/com/reggarf/mods/world_first_join_message/World_First_Join_Message.java
@@ -2,18 +2,11 @@ package com.reggarf.mods.world_first_join_message;
 
 import com.mojang.logging.LogUtils;
 import com.reggarf.mods.world_first_join_message.config.WFJMConfig;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.network.NetworkConstants;
-import org.apache.logging.log4j.LogManager;
-import org.slf4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -31,10 +24,5 @@ public class World_First_Join_Message {
 
         WFJMConfig.getCommon();
 
-        if (FMLEnvironment.dist == Dist.CLIENT) {
-            new PackMenuClient().load();
-        }
-        else LOGGER.error("Running on a dedicated server, disabling mod.");
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
