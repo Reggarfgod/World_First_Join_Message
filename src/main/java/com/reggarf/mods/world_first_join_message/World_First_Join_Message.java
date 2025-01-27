c0f15b22b637e36a086c1196ec33f1a08051438f
Anoop Singh <Reggerfgod@gmail.com>
2025-01-27 18:31:27 +0530

    event and config change

--- src/main/java/com/reggarf/mods/world_first_join_message/World_First_Join_Message.java
+++ src/main/java/com/reggarf/mods/world_first_join_message/World_First_Join_Message.java
@@ -1,9 +1,14 @@
 package com.reggarf.mods.world_first_join_message;
 
 import com.mojang.logging.LogUtils;
-import com.reggarf.mods.world_first_join_message.join.FirstJoinMessageHandler;
+import com.reggarf.mods.world_first_join_message.config.WFJMConfig;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.network.NetworkConstants;
+import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,17 +17,24 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(World_First_Join_Message.MODID)
+@Mod(World_First_Join_Message.MOD_ID)
 public class World_First_Join_Message {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "world_first_join_message";
+    public static final String MOD_ID = "world_first_join_message";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public World_First_Join_Message() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
-        //join register
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, FirstJoinMessageHandler.COMMON_SPEC);
+
+
+        WFJMConfig.getCommon();
+
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            new PackMenuClient().load();
+        }
+        else LOGGER.error("Running on a dedicated server, disabling mod.");
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
--- src/main/java/com/reggarf/mods/world_first_join_message/events/WFJMHandler.java
+++ src/main/java/com/reggarf/mods/world_first_join_message/events/WFJMHandler.java
@@ -1,4 +1,4 @@
-package com.reggarf.mods.world_first_join_message.join;
+package com.reggarf.mods.world_first_join_message.events;
 
 
 import com.reggarf.mods.world_first_join_message.World_First_Join_Message;
@@ -7,55 +7,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextColor;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = World_First_Join_Message.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class FirstJoinMessageHandler {
+import static com.reggarf.mods.world_first_join_message.config.WFJMCommonConfig.*;
 
-    // Configuration keys
-    private static ForgeConfigSpec.BooleanValue enabled;
-    private static ForgeConfigSpec.ConfigValue<String> welcomeMessage;
-    private static ForgeConfigSpec.ConfigValue<String> clickableUrl;
-    private static ForgeConfigSpec.ConfigValue<String> welcomeMessageColor;
-    private static ForgeConfigSpec.ConfigValue<String> clickableTextColor;
-
-    // Configuration setup
-    public static final ForgeConfigSpec COMMON_SPEC;
-
-    static {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        builder.push("welcome_message");
-
-        enabled = builder
-                .comment("Enable or disable the welcome message")
-                .define("enabled", true);
-
-        welcomeMessage = builder
-                .comment("The message to be displayed when a player joins the game for the first time")
-                .define("text", "THANKS TO OUR SPONSORS, ZAP-HOSTING! Support Reggarf And Save BIG - USE CODE Reggarf-1047 FOR 20% OFF Your ORDER!");
-
-        welcomeMessageColor = builder
-                .comment("The RGB color code for the welcome message.",
-                        "Examples: 'FF0000' for red, '00FF00' for green, '0000FF' for blue",
-                        "'FFFF00' for yellow, 'FF00FF' for magenta, '00FFFF' for cyan")
-                .define("color", "FFFFFF"); // Default color (white)
-
-        clickableUrl = builder
-                .comment("The URL to be opened when the player clicks the link")
-                .define("url", "https://zap-hosting.com/reggarf");
-
-        clickableTextColor = builder
-                .comment("The RGB color code for the clickable text '[Click here]',",
-                        "Examples: 'FF0000' for red, '00FF00' for green, '0000FF' for blue",
-                        "'FFFF00' for yellow, 'FF00FF' for magenta, '00FFFF' for cyan, 'FFFFFF' for white")
-                .define("clickableColor", "00FFAA"); // Default color (cyan)
-
-        builder.pop();
-        COMMON_SPEC = builder.build();
-    }
+@Mod.EventBusSubscriber(modid = World_First_Join_Message.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class WFJMHandler {
 
     @SubscribeEvent
     public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
